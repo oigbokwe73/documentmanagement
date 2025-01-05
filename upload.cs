5624d9fbd8a3e813096b45f2d3663d473a13ceb4
obinna <oigbokwe73@gmail.com>
2025-01-05 14:04:06 -0500

    Add project files.

---
+++ upload.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Collections.Specialized;
+using Xenhey.BPM.Core.Net8.Implementation;
+using Xenhey.BPM.Core.Net8;
+using Newtonsoft.Json;
+
+namespace documentmanagement
+{
+    public class upload
+    {
+        private readonly ILogger _logger;
+
+        public upload(ILogger<upload> logger)
+        {
+            _logger = logger;
+        }
+
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [Function("upload")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequest req)
+        {
+            _req = req;
+
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _req.Headers.ToList().ForEach(item => { nvc.Add(item.Key, item.Value.FirstOrDefault()); });
+            var response = new List<string>();
+            var formdata = await req.ReadFormAsync();
+            var file = req.Form.Files;
+            foreach (var item in file)
+            {
+                nvc.Add("FileName", item.FileName);
+                nvc.Add("FileContentType", item.ContentType);
+                nvc.Add("FileLegth", item.Length.ToString());
+                var results = orchrestatorService.Run(item.OpenReadStream());
+                response.Add(results);
+                nvc.Remove("FileName");
+                nvc.Remove("FileContentType");
+                nvc.Remove("FileLegth");
+            }
+
+            return resultSet(JsonConvert.SerializeObject(response));
+
+        }
+
+        private ActionResult resultSet(string reponsePayload)
+        {
+            var returnContent = new ContentResult();
+            var mediaSelectedtype = nvc.Get("Content-Type");
+            returnContent.Content = reponsePayload;
+            returnContent.ContentType = mediaSelectedtype;
+            return returnContent;
+        }
+        private IOrchestrationService orchrestatorService
+        {
+            get
+            {
+                return new RemoteOrchrestratorService(nvc);
+            }
+        }
+
+    }
+}
